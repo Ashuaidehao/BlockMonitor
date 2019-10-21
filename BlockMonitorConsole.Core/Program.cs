63c6f1ac60edb524b9b7ce23d4be9ae95ce9f2d5
Shuai <bfshm@qq.com>
2019-10-21 14:56:50 +0800

    neo3

---
+++ BlockMonitorConsole.Core/Program.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace BlockMonitor
+{
+    class Program
+    {
+
+        private const string MAIL_SUBJECT = "[Neo Wish]-3";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("开始监控");
+            Timer t = new Timer(new TimeSpan(0, 5, 0).TotalMilliseconds);
+            t.Elapsed += T_Elapsed;
+            t.Start();
+            T_Elapsed(null, null);
+            Console.ReadLine();
+        }
+
+        private static void T_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Status.HeightList.Clear();
+            var config = JObject.Parse(File.ReadAllText("config.json"));
+
+            foreach (var item in config["nodes"])
+            {
+                var h = Tools.GetBlockCount(item.ToString());
+                Console.WriteLine($"{item}\t{h}");
+                Status.HeightList.Add(h);
+            }
+            int height = Status.HeightList.Max();
+
+            if (Status.BlockCount > 0)
+            {
+                if (height == Status.BlockCount)
+                {
+                    var msg = $"NEO停止出块，超过{Math.Round((DateTime.Now - Status.Time).TotalMinutes)}分钟未出块";
+                    Console.WriteLine($"{msg}, { DateTime.Now.ToString()}");
+                    Tools.SendMail(msg, $"{MAIL_SUBJECT} 停止出块❗❗❗");
+                    return;
+                }
+
+                var timeSpan = Math.Round((DateTime.Now - Status.Time).TotalSeconds / (height - Status.BlockCount), 1);
+
+                if (timeSpan >= 35 && timeSpan < 300)
+                {
+                    var msg = $"NEO出块变慢，最近5分钟平均出块时间为{timeSpan}秒。<br />PS：异常区间：{Status.BlockCount}~{height}。";
+                    Console.WriteLine($"{msg.Replace("<br />", "\n")}, { DateTime.Now.ToString()}");
+                    Tools.SendMail(msg, $"{MAIL_SUBJECT}出块变慢❗");
+                    Status.BlockCount = height;
+                    Status.Time = DateTime.Now;
+                }
+                else
+                {
+                    Console.WriteLine($"出块正常，平均出块时间{timeSpan}秒 {height}, {DateTime.Now.ToString()}");
+                    Status.BlockCount = height;
+                    Status.Time = DateTime.Now;
+                }
+            }
+            else
+            {
+                Status.BlockCount = height;
+                Status.Time = DateTime.Now;
+                Console.WriteLine($"出块正常 {height}, {DateTime.Now.ToString()}");
+            }
+        }
+    }
+}
---
+++ BlockMonitorConsole.Core/Tools.cs
@@ -0,0 +1,101 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml;
+
+namespace BlockMonitor
+{
+    public static class Tools
+    {
+        public static string HttpPost(string Url, string postData, List<HttpHeader> HttpHeaders = null, int timeOut = 5000)
+        {
+            WebRequest request = WebRequest.Create(Url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            request.ContentLength = byteArray.Length;
+            request.Timeout = timeOut;
+            if (HttpHeaders != null && HttpHeaders.Count > 0)
+            {
+                foreach (var item in HttpHeaders)
+                {
+                    switch (item.Name)
+                    {
+                        case "Accept": break;
+                        case "Content-Type": request.ContentType = item.Value; break;
+                        default: request.Headers.Add(item.Name, item.Value); break;
+                    }
+                }
+            }
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+            WebResponse response = request.GetResponse();
+            dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            return responseFromServer;
+        }
+
+        public static void SendMail(string to, string subject, string body)
+        {
+            var config = JObject.Parse(File.ReadAllText("config.json"));
+            using (MailMessage mail = new MailMessage
+            {
+                From = new MailAddress(config["email"]["username"].ToString(), config["email"]["from"].ToString()),
+                Subject = subject,
+                BodyEncoding = Encoding.UTF8,
+                Body = body,
+                IsBodyHtml = true,
+            })
+            {
+                mail.To.Add(to);
+
+                using (SmtpClient smtp = new SmtpClient("smtp.office365.com", 587) {EnableSsl = true, UseDefaultCredentials = true, DeliveryMethod = SmtpDeliveryMethod.Network, Credentials = new NetworkCredential(config["email"]["username"].ToString(), config["email"]["password"].ToString())})
+                {
+                    try
+                    {
+                        smtp.Send(mail);
+                    }
+                    catch (SmtpException e)
+                    {
+                        Console.WriteLine("SmtpException" + e.Message);
+                    }
+                }
+            }
+        }
+
+        public static int GetBlockCount(string node)
+        {
+            string json;
+            try
+            {
+                json = HttpPost($"{node}", "{'jsonrpc': '2.0', 'method': 'getblockcount', 'params': [], 'id':   1}");
+                return (int)JObject.Parse(json)["result"] - 1;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+
+        }
+
+        public static void SendMail(string msg, string subject)
+        {
+            var config = JObject.Parse(File.ReadAllText("config.json"));
+            foreach (var item in config["contact"])
+            {
+                SendMail(item.ToString(), subject, msg);
+            }
+        }
+    }
+
+}
